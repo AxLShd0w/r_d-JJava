56e47a6eecc00cfefb1d6e60b5e8d013fc0d508b
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-19 20:04:37 +0200

    Lesson 4

--- src/com/ua/robot/lesson4/Main.java
+++ src/com/ua/robot/lesson4/Main.java
@@ -22,9 +22,18 @@ public class Main {
         System.out.println(str.charAt(lastIndex));
 
         String s2 = "first" + "second";
-        String s3 = Integer.parseInt("3") + (4 + 5);
-        System.out.println(s3);
+//        String s3 = Integer.parseInt("3") + (4 + 5);
+//        System.out.println(s3);
+//        str += "llo";
 
-        System.out.println(str.concat(""));
+//        System.out.println(str.concat(""));
+
+        System.out.println(str.substring(1, 6));
+        System.out.println(lowercase.indexOf("l"));
+        System.out.println(str.lastIndexOf("l"));
+
+
+
+
     }
 }

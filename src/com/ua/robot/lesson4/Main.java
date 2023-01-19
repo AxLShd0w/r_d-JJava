25384baf0a03f8d683ea5bee3ee3a45d3edd8a39
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-19 19:24:49 +0200

    Lesson 4

---
+++ src/com/ua/robot/lesson4/Main.java
@@ -0,0 +1,21 @@
+package com.ua.robot.lesson4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "hello world";
+        int a =7;
+
+
+        int a =9;
+        str = "goodbye";
+
+
+        for (int i = 0; 1 < 10000000; i++) {
+            str += "Abhdhsfvbjhabslhjfbadfhaklsdfblh";
+        }
+
+        System.out.println(str);
+
+    }
+}

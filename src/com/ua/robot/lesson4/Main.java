383b745e271ef98c120809c1e4fbda6e8f1f8f37
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-19 19:45:31 +0200

    Lesson 4

--- src/com/ua/robot/lesson4/Main.java
+++ src/com/ua/robot/lesson4/Main.java
@@ -1,21 +1,30 @@
 package com.ua.robot.lesson4;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-        String str = "hello world";
-        int a =7;
-
-
-        int a =9;
-        str = "goodbye";
-
-
-        for (int i = 0; 1 < 10000000; i++) {
-            str += "Abhdhsfvbjhabslhjfbadfhaklsdfblh";
-        }
+        String str = "Hello World";
 
         System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
 
+        String lowercase = str.toLowerCase();
+        String uppercase = str.toUpperCase();
+
+        System.out.println(str.charAt(6));
+
+        int lenght = str.length();
+        int lastIndex = lenght -1;
+        System.out.println(str.charAt(lastIndex));
+
+        String s2 = "first" + "second";
+        String s3 = Integer.parseInt("3") + (4 + 5);
+        System.out.println(s3);
+
+        System.out.println(str.concat(""));
     }
 }

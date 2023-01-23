1fb1bc8827edf93b5ee1af5b89bfac08aa4f3c3f
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-23 22:10:15 +0200

    Lesson 5

---
+++ src/com/ua/robot/lesson5/Main.java
@@ -0,0 +1,12 @@
+package com.ua.robot.lesson5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 11;
+
+        if (number %2 == 0 && number > 0) {
+            System.out.println(number + " is even");
+        }
+    }
+}
